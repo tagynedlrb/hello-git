c4116e6c8ea0e40e5e1534119c6cc0c41385a19e
EC2 Default User <ec2-user@ip-172-31-12-191.ap-northeast-2.compute.internal>
2020-11-29 04:04:48 +0900

    New commit by carol

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
 		System.out.println("Changes after Firt commit");
+		//Something for pull request
 	}
 }
